12d3273dd52c3d6d6befaa25f58e558c51c58ff1
Dejan Jarc <jarcdejan5@gmail.com>
2025-10-21 16:33:28 +0200

    Init

---
+++ src/test/kotlin/traversium/tripservice/TripServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package traversium.tripservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TripServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
