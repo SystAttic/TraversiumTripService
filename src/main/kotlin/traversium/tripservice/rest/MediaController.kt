626b28f34b5a9d2060f4fd33d3c27e46a328e083
Dejan Jarc <jarcdejan5@gmail.com>
2025-10-23 23:37:42 +0200

    add: MediaController Swagger docs

--- src/main/kotlin/traversium/tripservice/rest/MediaController.kt
+++ src/main/kotlin/traversium/tripservice/rest/MediaController.kt
@@ -1,6 +1,11 @@
 package traversium.tripservice.rest
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import traversium.tripservice.dto.MediaDto
@@ -13,23 +18,111 @@ class MediaController(
 ) {
 
     @GetMapping
+    @Operation(
+        summary = "Get media by album id",
+        description = "Get media by album id",
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Media returned",
+                content = [Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = Schema(implementation = MediaDto::class)
+                )]
+            ),
+            ApiResponse(
+                responseCode = "400",
+                description = "Bad request"
+            ),
+            ApiResponse(
+                responseCode = "500",
+                description = "Internal server error"
+            )
+        ]
+    )
     fun getMediaForAlbum(@PathVariable albumId: Long): List<MediaDto> =
         mediaService.getMediaForAlbum(albumId)
 
     @GetMapping("/{mediaId}")
-    fun getMediaById(@PathVariable mediaId: Long): MediaDto =
+    @Operation(
+        summary = "Get media by mediaId from album by albumId",
+        description = "Get media by mediaId from album by id",
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Media returned",
+                content = [Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = Schema(implementation = MediaDto::class)
+                )]
+            ),
+            ApiResponse(
+                responseCode = "400",
+                description = "Bad request"
+            ),
+            ApiResponse(
+                responseCode = "500",
+                description = "Internal server error"
+            )
+        ]
+    )
+    fun getMediaById(@PathVariable mediaId: Long, @PathVariable albumId: Long): MediaDto =
         mediaService.getMediaById(mediaId)
 
     @PostMapping
-    fun addMedia(
+    @Operation(
+        summary = "Add media to album by albumId",
+        description = "Add media to album by albumId",
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Media added to album by albumId",
+                content = [Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = Schema(implementation = MediaDto::class)
+                )]
+            ),
+            ApiResponse(
+                responseCode = "400",
+                description = "Bad request"
+            ),
+            ApiResponse(
+                responseCode = "500",
+                description = "Internal server error"
+            )
+        ]
+    )
+    fun addMediaToAlbum(
         @PathVariable albumId: Long,
         @RequestBody dto: MediaDto
     ): ResponseEntity<MediaDto> =
-        ResponseEntity.status(HttpStatus.CREATED).body(mediaService.addMedia(albumId, dto))
+        ResponseEntity.status(HttpStatus.CREATED).body(mediaService.addMediaToAlbum(albumId, dto))
 
     @DeleteMapping("/{mediaId}")
-    fun deleteMedia(@PathVariable mediaId: Long): ResponseEntity<Void> {
-        mediaService.deleteMedia(mediaId)
+    @Operation(
+        summary = "Delete media by mediaId from album",
+        description = "Delete media by mediaId",
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Media deleted",
+                content = [Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = Schema(implementation = MediaDto::class)
+                )]
+            ),
+            ApiResponse(
+                responseCode = "400",
+                description = "Bad request"
+            ),
+            ApiResponse(
+                responseCode = "500",
+                description = "Internal server error"
+            )
+        ]
+    )
+    fun deleteMediaFromAlbum(@PathVariable mediaId: Long, @PathVariable albumId: Long): ResponseEntity<Void> {
+        mediaService.deleteMediaFromAlbum(albumId, mediaId)
         return ResponseEntity.noContent().build()
     }
 }
