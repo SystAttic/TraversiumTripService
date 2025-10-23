5d8c6ae3809fb8a498e7d112765fec40cbdf2e99
Dejan Jarc <jarcdejan5@gmail.com>
2025-10-24 00:11:49 +0200

    fix: some repository stuff

--- src/main/kotlin/traversium/tripservice/db/repository/AlbumRepository.kt
+++ src/main/kotlin/traversium/tripservice/db/repository/AlbumRepository.kt
@@ -1,10 +1,17 @@
 package traversium.tripservice.db.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import traversium.tripservice.db.model.Album
+import traversium.tripservice.db.model.Media
 
 @Repository
 interface AlbumRepository : JpaRepository<Album, Long> {
-    fun findByTripId(tripId: Long): List<Album>
+
+    @Query("select a.media from Album a where a.albumId = :albumId")
+    fun getMedia(@Param("albumId") albumId: Long): List<Media>
+
+    fun getByTripId(@Param("tripId") tripId: Long): List<Album>
 }
--- src/main/kotlin/traversium/tripservice/db/repository/MediaRepository.kt
+++ src/main/kotlin/traversium/tripservice/db/repository/MediaRepository.kt
@@ -1,10 +1,19 @@
 package traversium.tripservice.db.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import traversium.tripservice.db.model.Media
 
 @Repository
 interface MediaRepository : JpaRepository<Media, Long> {
-    fun findByAlbumId(albumId: Long): List<Media>
+    @Query("SELECT m FROM Media m WHERE m.album.albumId = :albumId")
+    fun findByAlbumId(@Param("albumId") albumId: Long): List<Media>
+
+    @Query("SELECT m FROM Media m WHERE m.mediaId = :mediaId AND m.album.albumId = :albumId")
+    fun findByMediaIdAndAlbumId(
+        @Param("mediaId") mediaId: Long,
+        @Param("albumId") albumId: Long
+    ): Media?
 }
