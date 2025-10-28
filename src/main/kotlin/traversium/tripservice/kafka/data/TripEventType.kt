7a41954fcd44adf38f2a592a6af50a599d551221
Dejan Jarc <jarcdejan5@gmail.com>
2025-10-28 12:53:08 +0100

    Revert "add: Trip Exceptions"
    This reverts commit e78c9e24fc6730b3874ab779a9aab6eded56fd26.

--- src/main/kotlin/traversium/tripservice/kafka/data/TripEventType.kt
+++ src/main/kotlin/traversium/tripservice/kafka/data/TripEventType.kt
@@ -3,9 +3,5 @@ package traversium.tripservice.kafka.data
 enum class TripEventType {
     TRIP_CREATED,
     TRIP_UPDATED,
-    TRIP_DELETED,
-    COLLABORATOR_ADDED,
-    COLLABORATOR_DELETED,
-    VIEWER_ADDED,
-    VIEWER_DELETED,
+    TRIP_DELETED
 }
