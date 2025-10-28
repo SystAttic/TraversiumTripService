e78c9e24fc6730b3874ab779a9aab6eded56fd26
Dejan Jarc <jarcdejan5@gmail.com>
2025-10-28 12:52:49 +0100

    add: Trip Exceptions

--- src/main/kotlin/traversium/tripservice/kafka/data/TripEventType.kt
+++ src/main/kotlin/traversium/tripservice/kafka/data/TripEventType.kt
@@ -3,5 +3,9 @@ package traversium.tripservice.kafka.data
 enum class TripEventType {
     TRIP_CREATED,
     TRIP_UPDATED,
-    TRIP_DELETED
+    TRIP_DELETED,
+    COLLABORATOR_ADDED,
+    COLLABORATOR_DELETED,
+    VIEWER_ADDED,
+    VIEWER_DELETED,
 }
