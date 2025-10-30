776f88c061f2c6e5df33d933e8f4e8ea0b9a9a8a
Dejan Jarc <jarcdejan5@gmail.com>
2025-10-30 17:49:57 +0100

    add: Album Exceptions

--- src/main/kotlin/traversium/tripservice/exceptions/AlbumExceptions.kt
+++ src/main/kotlin/traversium/tripservice/exceptions/AlbumExceptions.kt
@@ -1,3 +1,5 @@
 package traversium.tripservice.exceptions
 
 class AlbumNotFoundException(id: Long) : RuntimeException("Album with ID $id not found")
+class AlbumAlreadyExistsException(id: Long) : RuntimeException("Album with ID $id already exists.")
+class AlbumWithoutMediaException(id: Long) : RuntimeException("Album with ID $id has no media.")
